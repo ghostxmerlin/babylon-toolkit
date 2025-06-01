10c7dbc9377ae74eefb18d7988e7745089207309
Tong Li <supertong@users.noreply.github.com>
2025-06-01 21:51:44 +1000

    feat: try bump packages (#21)

--- packages/example-lib/src/lib/example-lib.ts
+++ packages/example-lib/src/lib/example-lib.ts
@@ -1,3 +1,3 @@
 export function exampleLib(): string {
-  return 'example-lib';
+  return 'example-lib-new';
 }
--- packages/example-lib3/src/lib/example-lib3.ts
+++ packages/example-lib3/src/lib/example-lib3.ts
@@ -1,5 +1,5 @@
 import { exampleLib2 } from '@babylonlabs-io/example-lib2';
 
 export function exampleLib3(): string {
-  return exampleLib2() + 'example-lib3';
+  return exampleLib2() + 'example-lib3-new';
 }
