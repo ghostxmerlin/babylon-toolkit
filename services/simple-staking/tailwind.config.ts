a97ebf5c91992670f2214f41b35fc5d5f07d72b5
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2025-09-10 09:47:26 +0700

    fix(packages): add core-ui package to tainwind content array (#164)

--- services/simple-staking/tailwind.config.ts
+++ services/simple-staking/tailwind.config.ts
@@ -4,12 +4,16 @@ import { screenBreakPoints } from "./src/ui/common/config/screen-breakpoints";
 
 const coreUIConfig = require("@babylonlabs-io/core-ui/tailwind");
 
-const config: Config = {
+const config = {
   presets: [coreUIConfig],
-  content: ["./src/ui/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/babylon-core-ui/src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/babylon-wallet-connector/src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
-    screens: screenBreakPoints,
+      screens: screenBreakPoints,
   },
 };
 
