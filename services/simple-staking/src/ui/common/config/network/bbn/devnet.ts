6e908b0efd8ab5cb793cc8b1d5b2c5a239948f90
Crypto Minion <154598612+jrwbabylonlab@users.noreply.github.com>
2025-09-25 09:55:08 +1000

    fix(packages): update devnet rpc url (#245)

--- services/simple-staking/src/ui/common/config/network/bbn/devnet.ts
+++ services/simple-staking/src/ui/common/config/network/bbn/devnet.ts
@@ -3,13 +3,13 @@ import { getUrlFromEnv } from "./urlUtils";
 export const BBN_DEVNET_RPC_URL = getUrlFromEnv(
   process.env.NEXT_PUBLIC_BABY_RPC_URL,
   "http://localhost:3000",
-  "https://rpc.devnet.babylonlabs.io/",
+  "https://rpc.canon-devnet.babylonlabs.io/",
 );
 
 export const BBN_DEVNET_LCD_URL = getUrlFromEnv(
   process.env.NEXT_PUBLIC_BABY_LCD_URL,
   "http://localhost:1317",
-  "https://lcd-dapp.devnet.babylonlabs.io",
+  "https://lcd.canon-devnet.babylonlabs.io",
 );
 
 export const bbnDevnet = {
