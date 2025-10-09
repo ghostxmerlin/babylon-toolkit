844e877b887442477cec68c24ad3d85212b871cd
Filippos Malandrakis <35352222+filippos47@users.noreply.github.com>
2025-10-09 11:57:58 +0300

    chore: Update Testnet-5 sunset date (#327)

--- services/simple-staking/src/ui/common/components/Banner/Banner.tsx
+++ services/simple-staking/src/ui/common/components/Banner/Banner.tsx
@@ -36,7 +36,7 @@ export const Banner = () => {
             <PiWarningOctagonFill />
             <Text variant="body1">
               <strong>
-                Testnet-5 will be sunset on October 9, 2025. It will be
+                Testnet-5 will be sunset on October 13, 2025. It will be
                 succeeded by Testnet-6.
               </strong>
               <br />
